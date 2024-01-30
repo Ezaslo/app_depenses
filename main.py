16b31ceb934ed80e86e692343eb426126054a3e9
Ezaslo <enzo.t.anselmo@gmail.com>
2024-01-30 13:42:24 +0100

    Ajouts des fonctionnalités mois et affichage depenses et prelevements

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import datetime
 import json
 
-argent_net = 0
-complement_revenu = 0
+salaire = 0
 prime = 0
+complement_revenu = 0
 depenses = 0
-argent_disponible = 0
+argent_net = 0
 prelevements_globaux = []
 reserves_input = 0
+reserves = 0  
+retrait = 0
+reserves_inputneg = 0
+reserves_inputpos = 0
+depense = 0
 
 
-def enregistrer_donnees_texte():
+
+def enregistrer_donnees_texte(nom_fichier):
     global salaire, prime, prelevements_globaux, argent_net, depenses_possibles, complement_revenu, depenses, argent_disponible, reserves, reserves_input
     donnees = {
         "salaire": salaire,
@@ -22,17 +28,17 @@ def enregistrer_donnees_texte():
         "depenses": depenses,
         "argent_disponible": argent_disponible,
         "reserves": reserves,
-        "reserves_input": reserves_input  # Ajout de la variable reserves_input
+        "reserves_input": reserves_input 
     }
-    with open("donnees.json", "w") as fichier:
+    with open(f"{nom_fichier}.json", "w") as fichier:
         json.dump(donnees, fichier, indent=4)
 
 
 
-def lire_donnees_texte():
+def lire_donnees_texte(nom_fichier):
     global salaire, prelevements_globaux, argent_net, depenses_possibles, prime, complement_revenu, depenses, argent_disponible, reserves
     try:
-        with open("donnees.json", "r") as fichier:
+        with open(f"{nom_fichier}.json", "r") as fichier:
             donnees = json.load(fichier)
             salaire = donnees.get("salaire", 0)
             prelevements_globaux = donnees.get("prelevements_globaux", [])
@@ -45,7 +51,6 @@ def lire_donnees_texte():
             reserves = donnees.get("reserves", 0)
     except FileNotFoundError:
         print("Fichier de données introuvable. Création d'un nouveau fichier.")
-        # Initialiser toutes les variables à des valeurs par défaut
         salaire = 0
         prelevements_globaux = []
         argent_net = 0
@@ -55,15 +60,20 @@ def lire_donnees_texte():
         depenses = 0
         argent_disponible = 0
         reserves = 0
-        enregistrer_donnees_texte()  # Appelle la fonction pour créer le fichier avec les valeurs par défaut
+        enregistrer_donnees_texte(nom_fichier)  
     except json.JSONDecodeError:
         print("Erreur lors de la lecture des données. Format de fichier incorrect.")
     except Exception as e:
         print(f"Une erreur s'est produite lors de la lecture du fichier : {e}")
 
+def charger_donnees_mois(nom_fichier):
+    mois = input("entrez le mois des donnees à charger ex Janvier-2024")
+    lire_donnees_texte(mois)
 
 
-def menu_salaire():
+
+
+def menu_salaire(nom_fichier):
     while True:
         print("\nMenu Salaire\n")
         print("1. Entrer le salaire reçu")
@@ -72,12 +82,11 @@ def menu_salaire():
         print("4. Corriger une erreur")
         print("5. Quitter\n")
         try:
-            # Utiliser directement calculer_argent_disponible() dans le print peut lever une exception si mal configuré
             argent_dispo = calculer_argent_disponible()  
             print(f"Vous avez {argent_dispo} € de disponible")
         except Exception as e:
             print(f"Une erreur est survenue lors du calcul de l'argent disponible: {e}")
-            # Option de gestion d'erreur, par exemple, continuer ou quitter
+        
             continue
 
         try:
@@ -87,13 +96,13 @@ def menu_salaire():
             continue
 
         if choix == 1:
-            salaire_reçu()
+            salaire_reçu(nom_fichier)
         elif choix == 2:
-            prime_reçue()
+            prime_reçue(nom_fichier)
         elif choix == 3:
-            complement()
+            complement(nom_fichier)
         elif choix == 4:
-            corriger_erreur()
+            corriger_erreur(nom_fichier)
         elif choix == 5:
             print("Quitter le menu Salaire.")
             break
@@ -102,75 +111,73 @@ def menu_salaire():
 
 
 
-def salaire_reçu():
+def salaire_reçu(nom_fichier):
     global salaire
     global argent_net
     while True:
         try:
             salaire_input = input("Combien avez-vous reçu ? :")
-            salaire_temp = float(salaire_input)  # Tente de convertir l'entrée en float
+            salaire_temp = float(salaire_input) 
             if salaire_temp < 0:
                 print("Le montant du salaire ne peut pas être négatif. Veuillez entrer un montant valide.")
                 continue
             salaire = salaire_temp
-            break  # Sortie de la boucle si la conversion est réussie et le salaire est positif
+            break  
         except ValueError:
             print("Veuillez entrer un nombre valide. Par exemple, 2500.50")
-            # La boucle continue, demandant à l'utilisateur de réessayer
+            
 
     print("Prise en compte de votre salaire...")
-    argent_net += salaire  # Met à jour argent_net avec le nouveau salaire
+    argent_net += salaire  
 
-    enregistrer_donnees_texte()  # Sauvegarde les données après la mise à jour
+    enregistrer_donnees_texte(nom_fichier)  
 
 
-def prime_reçue():
+def prime_reçue(nom_fichier):
     global prime
     global argent_net
 
     while True:
         try:
             prime_input = input("Combien avez-vous reçu en prime ? :")
-            prime_temp = float(prime_input)  # Essaie de convertir l'entrée en float
+            prime_temp = float(prime_input)  
             if prime_temp < 0:
                 print("Le montant de la prime ne peut pas être négatif. Veuillez entrer un montant valide.")
                 continue
             prime = prime_temp
-            break  # Sortie de la boucle si la conversion est réussie et la prime est positive
+            break  
         except ValueError:
             print("Veuillez entrer un nombre valide. Par exemple, 500.00")
-            # La boucle continue, demandant à l'utilisateur de réessayer
-
+            
     print("Prise en compte de votre prime...")
-    argent_net += prime  # Met à jour argent_net avec la nouvelle prime
+    argent_net += prime  
 
-    enregistrer_donnees_texte()  # Sauvegarde les données après la mise à jour
+    enregistrer_donnees_texte(nom_fichier)  
 
-def complement():
+def complement(nom_fichier):
     global argent_net
     global complement_revenu
 
     while True:
         try:
             complement_input = input("Combien avez-vous eu en complément ? :")
-            complement_temp = float(complement_input)  # Essaie de convertir l'entrée en float
+            complement_temp = float(complement_input)  
             if complement_temp < 0:
                 print("Le montant du complément ne peut pas être négatif. Veuillez entrer un montant valide.")
                 continue
             complement_revenu = complement_temp
-            break  # Sortie de la boucle si la conversion est réussie et le complément est positif
+            break  
         except ValueError:
             print("Veuillez entrer un nombre valide. Par exemple, 300.00")
-            # La boucle continue, demandant à l'utilisateur de réessayer
-
+            
     print("Prise en compte de votre complément...")
-    argent_net += complement_revenu  # Met à jour argent_net avec le nouveau complément
+    argent_net += complement_revenu  
 
-    enregistrer_donnees_texte()  # Sauvegarde les données après la mise à jour
+    enregistrer_donnees_texte(nom_fichier)  
 
 
 
-def corriger_erreur():
+def corriger_erreur(nom_fichier):
     global argent_net
 
     while True:
@@ -184,18 +191,18 @@ def corriger_erreur():
             elif erreur > argent_disponible_apres_prelevements:
                 print(f"Le montant ne peut pas dépasser votre argent disponible après prélèvements, qui est de {argent_disponible_apres_prelevements} €.")
             else:
-                break  # Sortie de la boucle si l'entrée est valide
+                break  
         except ValueError:
             print("Veuillez entrer un nombre valide.")
 
-    argent_net -= erreur  # Applique la correction
+    argent_net -= erreur  
     print(f"Vous avez donc {calculer_argent_disponible()} € disponible après cette correction.")
 
-    enregistrer_donnees_texte()
+    enregistrer_donnees_texte(nom_fichier)
 
 
 
-def menu_prelevements():
+def menu_prelevements(nom_fichier):
     while True:
         print("\nMenu des prélèvements\n")
         print("1. Ajouter un prélèvement")
@@ -209,15 +216,15 @@ def menu_prelevements():
             choix = int(choix)
 
             if choix == 1:
-                ajouter_prelevements()
+                ajouter_prelevements(nom_fichier)
             elif choix == 2:
                 nom_prelevement = input("Entrez le nom exact du prélèvement que vous voulez supprimer ou entrez 'q' pour quitter : ")
                 if nom_prelevement.lower() == "q":
                     print("Retour au menu principal...")
                 else:
-                    supprimer_prelevement(nom_prelevement)
+                    supprimer_prelevement(nom_prelevement,nom_fichier)
             elif choix == 3:
-                afficher_prelevements()
+                afficher_prelevements(nom_fichier) 
             elif choix == 4:
                 print("Retour au menu principal...")
                 break
@@ -231,7 +238,7 @@ def menu_prelevements():
 
 
 
-def ajouter_prelevements():
+def ajouter_prelevements(nom_fichier):
     while True:
         nom = input("Entrez le nom du prélèvement ou 'q' pour quitter : ").strip().lower()
         
@@ -250,11 +257,11 @@ def ajouter_prelevements():
             
             if montant < 0:
                 print("Le montant du prélèvement ne peut pas être négatif. Réessayez.")
-                continue  # Redémarre la boucle pour permettre une nouvelle entrée
+                continue  
                 
             prelevements_globaux.append({'nom': nom, 'montant': montant})
             print(f"Prélèvement '{nom}' de {montant} € ajouté avec succès.")
-            enregistrer_donnees_texte()
+            enregistrer_donnees_texte(nom_fichier)
             break
         
         except ValueError:
@@ -264,22 +271,22 @@ def ajouter_prelevements():
 
 
 
-def supprimer_prelevement(nom_prelevement):
+def supprimer_prelevement(nom_prelevement,nom_fichier):
     global prelevements_globaux
-    prelevements_avant = prelevements_globaux.copy()  # Crée une copie de la liste actuelle
+    prelevements_avant = prelevements_globaux.copy()  
 
-    # Utilise une liste en compréhension pour supprimer le prélèvement avec le nom spécifié
+    
     prelevements_globaux = [prelevement for prelevement in prelevements_globaux if prelevement['nom'] != nom_prelevement.lower()]
 
-    # Vérifie si des prélèvements ont été supprimés
+    
     if len(prelevements_avant) == len(prelevements_globaux):
         print(f"Aucun prélèvement avec le nom '{nom_prelevement}' trouvé.")
     else:
         print(f"Prélèvement '{nom_prelevement}' supprimé si existant.")
-        enregistrer_donnees_texte()
+        enregistrer_donnees_texte(nom_fichier)
 
 
-def afficher_prelevements():
+def afficher_prelevements(nom_fichier):
     if prelevements_globaux:
         print("\nListe des prélèvements :")
         for prelevement in prelevements_globaux:
@@ -294,11 +301,11 @@ def afficher_prelevements():
 
 
 
-def calcul_depenses():
-    global argent_net, prelevements_globaux  # Pas besoin de 'argent_disponible' comme variable globale
+def calcul_depenses(nom_fichier):
+    global argent_net, prelevements_globaux ,depense,depenses
     
     while True:
-        argent_disponible = calculer_argent_disponible()  # Calculez dynamiquement
+        argent_disponible = calculer_argent_disponible()  
         print(f"Vous avez {argent_disponible} € de disponible.")
         depense_input = input("Combien as-tu dépensé ? (Appuyez sur 'q' pour quitter) : ")
         
@@ -307,10 +314,10 @@ def calcul_depenses():
 
         try:
             depense = float(depense_input)
-            if depense <= argent_disponible:  # Vérifiez si la dépense est possible
-                argent_net -= depense  # Appliquez la dépense
-                print(f"Après dépense, vous avez {calculer_argent_disponible()} € de disponible.")
-                enregistrer_donnees_texte()
+            if depense <= argent_disponible:  
+                argent_net -= depense  
+                depenses += depense
+                enregistrer_donnees_texte(nom_fichier)
             else:
                 print("Dépense refusée. Vous n'avez pas assez d'argent disponible.")
         except ValueError:  
@@ -319,24 +326,30 @@ def calcul_depenses():
 
 
 
-def remise_a_zero():
-    global salaire, prime, argent_net, depenses_possibles, prelevements_globaux, complement_revenu
-
+def remise_a_zero(nom_fichier):
+    global salaire, prime, argent_net, depenses_possibles, prelevements_globaux, complement_revenu,depenses,depense
+    mois = input("Entrez le mois : ")
+    nom_fichier = mois
     salaire = 0
     argent_net = 0
     depenses_possibles = 0
     prelevements_globaux = []
     complement_revenu = 0
+    depenses = 0
+    depense = 0
 
     print("Données réinitialisées. Bienvenue dans votre nouveau mois, ne dépensez pas trop :)")
 
-    enregistrer_donnees_texte()
+    enregistrer_donnees_texte(mois)
 
 
 def calculer_argent_disponible():
     global argent_net, prelevements_globaux, reserves_input
+
     total_prelevements = sum(prelevement['montant'] for prelevement in prelevements_globaux)
-    argent_disponible = argent_net - total_prelevements - reserves_input
+    argent_disponible = argent_net - total_prelevements 
+
+
     return argent_disponible
 
 
@@ -380,8 +393,8 @@ def calcul_objectif_argent():
         except Exception as e:
             print(f"Une erreur inattendue est survenue : {e}")
 
-def reserves_argent():
-    global reserves, argent_disponible, reserves_input
+def reserves_argent(nom_fichier):
+    global reserves, argent_disponible,reserves_input,argent_net
     
     print(f"Vous avez {reserves} € de côté.")
     
@@ -396,30 +409,43 @@ def reserves_argent():
             break
         elif action == '1':
             try:
-                reserves_input += float(input("Combien d'argent voulez-vous mettre de côté ce mois-ci : "))
-                reserves += reserves_input
-                argent_disponible -= reserves_input  # Met à jour l'argent disponible après l'ajout
+                reserves_input = float(input("Combien d'argent voulez-vous mettre de côté ce mois-ci : "))
+                
+                reserves = reserves + reserves_input
+                argent_net = argent_net - reserves_input
+            
                 print(f"Votre nouveau solde de côté est de {reserves} €.")
+                enregistrer_donnees_texte(nom_fichier)
+                reserves_input = 0
             except ValueError:
                 print("Erreur : Veuillez entrer un montant valide.")
+
+
         elif action == '2':
             try:
-                retrait = float(input("Combien d'argent voulez-vous retirer : "))
-                if retrait <= reserves:
-                    reserves_input -= retrait
-                    reserves -= retrait
-                    argent_disponible += retrait  # Met à jour l'argent disponible après le retrait
-                    print(f"Vous avez retiré {retrait} €. Votre nouveau solde de côté est de {reserves} €.")
-                else:
-                    print("Vous n'avez pas suffisamment d'argent en réserve.")
+                reserves_input = float(input("Combien d'argent voulez-vous retirer: "))
+                
+                reserves = reserves - reserves_input
+                argent_net = argent_net + reserves_input
+            
+                print(f"Votre nouveau solde de côté est de {reserves} €.")
+                enregistrer_donnees_texte(nom_fichier)
+                
             except ValueError:
                 print("Erreur : Veuillez entrer un montant valide.")
 
 
 
+def afficher_totals(nom_fichier):
+    total_prelevements = sum(prelevement['montant'] for prelevement in prelevements_globaux)
+    total_depenses = depenses
+
+    print(f"\nTotal des prélèvements pour le mois : {total_prelevements} €")
+    print(f"Total des dépenses pour le mois : {total_depenses} €\n")
+    print(f"Total entrés d'argent pour le mois {salaire + prime + complement_revenu} €")
 
 
-def menu():
+def menu(nom_fichier):
     while True:
         print("\nMenu Principal\n")
         print("Options :")
@@ -429,6 +455,8 @@ def menu():
         print("4. Ajouter ou supprimer une dépense")
         print("5. Commencer un nouveau mois / Remise à zéro")
         print("6. Ajouter / Supprimer de l'argent de côté")
+        print("7. Charger les données d'un ancien mois")
+        print("8. Afficher vos prélèvements et depenses")
         print("0. Quitter le programme\n")
 
         argent_dispo = calculer_argent_disponible()  
@@ -440,17 +468,21 @@ def menu():
             print("Merci d'avoir utilisé le programme. Au revoir !")
             break
         elif choix == "1":
-            menu_salaire()
+            menu_salaire(nom_fichier)
         elif choix == "2":
-            menu_prelevements()
+            menu_prelevements(nom_fichier)
         elif choix == "3":
             calcul_objectif_argent()
         elif choix == "4":
-            calcul_depenses()
+            calcul_depenses(nom_fichier)
         elif choix == "5":
-            remise_a_zero()
+            remise_a_zero(nom_fichier)
         elif choix == "6":
-            reserves_argent()
+            reserves_argent(nom_fichier)
+        elif choix =='7':
+            charger_donnees_mois(nom_fichier)
+        elif choix == '8':
+            afficher_totals(nom_fichier)
         else:
             print("Option non valide. Veuillez choisir une option valide (1-5) ou 0 pour quitter.")
 
@@ -458,12 +490,20 @@ def menu():
 
 def main():
     print("Bonjour et Bienvenue à toi\n")
-    lire_donnees_texte()
-    menu()
+    
+    mois_valides = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+    
+    while True:
+        nom_fichier = input("Entrez le nom du mois en minuscule et sans accents ni ponctuation : ").strip().lower()
+        
+        if nom_fichier in mois_valides:
+            break
+        else:
+            print("Mois invalide. Veuillez entrer un mois valide.")
+
+    lire_donnees_texte(nom_fichier)
+    menu(nom_fichier)
+
 
 if __name__ == "__main__":
     main()
-
-
-    #a faire : corriger erreur enregistrement de quand je met de sous de cote
-
