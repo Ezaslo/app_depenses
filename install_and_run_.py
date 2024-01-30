def77ada31092ae822fde596b85041fd731070e6
Ezaslo <enzo.t.anselmo@gmail.com>
2024-01-30 16:54:39 +0100

    Ajouts des fonctionnalités mois et affichage depenses et prelevements

--- install_and_run_.py
+++ install_and_run_.py
@@ -9,24 +9,21 @@ import win32com.client
 subprocess.call(['pip', 'install', 'pywin32'])
 
 # URL de votre fichier main.py dans la branche spécifique
-github_main_url = 'https://raw.githubusercontent.com/Ezaslo/app_depenses/V3/main.py'
+github_main_url = 'https://raw.githubusercontent.com/Ezaslo/app_depenses/V2/main.py'
 
 # Chemin où le code sera téléchargé
 download_path = os.path.expanduser('~')
 
-# Répertoire d'installation de l'application
-install_path = "C:/programmes/Anselmo Studio"
-
-# Vérifiez si le répertoire d'installation existe, et s'il existe, supprimez-le
-if os.path.exists(install_path):
-    shutil.rmtree(install_path)
-
 # Téléchargez main.py depuis GitHub
 urllib.request.urlretrieve(github_main_url, os.path.join(download_path, 'main.py'))
 
-# Créez le répertoire d'installation s'il n'existe pas
-if not os.path.exists(install_path):
-    os.makedirs(install_path)
+# Répertoire d'installation de l'application modifié
+install_path = "C:/programmes/Anselmo Studio"
+
+# Si le répertoire d'installation existe déjà, supprimez-le et recréez-le
+if os.path.exists(install_path):
+    shutil.rmtree(install_path)
+os.makedirs(install_path)
 
 # Installez PyInstaller
 subprocess.call(['pip', 'install', 'pyinstaller'])
@@ -35,15 +32,14 @@ subprocess.call(['pip', 'install', 'pyinstaller'])
 main_py = os.path.join(download_path, 'main.py')
 subprocess.call(['pyinstaller', '--onefile', '--distpath', install_path, main_py])
 
-# Vérifiez si le raccourci existe déjà sur le bureau, et s'il existe, supprimez-le
+# Créez un raccourci sur le bureau de l'utilisateur
 desktop = Path(os.path.join(os.path.expanduser('~'), 'Desktop'))
-shortcut_path = desktop / 'Gestion_Budget.lnk'
+shortcut_path = desktop / 'Gestion_Budget.lnk'  # Nom du raccourci
+target_path = os.path.join(install_path, 'main.exe')
 
+# Si le raccourci existe déjà, supprimez-le avant de le recréer
 if os.path.exists(shortcut_path):
     os.remove(shortcut_path)
-
-# Créez un raccourci sur le bureau de l'utilisateur
-target_path = os.path.join(install_path, 'main.exe')
 
 shell = win32com.client.Dispatch("WScript.Shell")
 shortcut = shell.CreateShortCut(str(shortcut_path))
