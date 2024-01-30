c9686195d3ea20fd9c1426b1f98b117acef25382
Ezaslo <enzo.t.anselmo@gmail.com>
2024-01-30 14:53:31 +0100

    Ajouts des fonctionnalités mois et affichage depenses et prelevements

--- install_and_run_.py
+++ install_and_run_.py
@@ -1,8 +1,12 @@
 import os
 import subprocess
 import urllib.request
-import json
+from pathlib import Path
 import shutil
+import win32com.client
+
+# Assurez-vous que pywin32 est installé pour la gestion des raccourcis
+subprocess.call(['pip', 'install', 'pywin32'])
 
 # URL de votre fichier main.py dans la branche spécifique
 github_main_url = 'https://raw.githubusercontent.com/Ezaslo/app_depenses/V2/main.py'
@@ -10,27 +14,45 @@ github_main_url = 'https://raw.githubusercontent.com/Ezaslo/app_depenses/V2/main
 # Chemin où le code sera téléchargé
 download_path = os.path.expanduser('~')
 
+# Répertoire d'installation de l'application
+install_path = "C:/programmes/Anselmo Studio"
+
+# Vérifiez si le répertoire d'installation existe, et s'il existe, supprimez-le
+if os.path.exists(install_path):
+    shutil.rmtree(install_path)
 
 # Téléchargez main.py depuis GitHub
 urllib.request.urlretrieve(github_main_url, os.path.join(download_path, 'main.py'))
 
+# Créez le répertoire d'installation s'il n'existe pas
+if not os.path.exists(install_path):
+    os.makedirs(install_path)
+
 # Installez PyInstaller
 subprocess.call(['pip', 'install', 'pyinstaller'])
 
-# Exécutez PyInstaller sur main.py pour générer l'exécutable
+# Exécutez PyInstaller sur main.py pour générer l'exécutable directement dans le répertoire d'installation
 main_py = os.path.join(download_path, 'main.py')
+subprocess.call(['pyinstaller', '--onefile', '--distpath', install_path, main_py])
 
-subprocess.call(['pyinstaller', main_py])
+# Vérifiez si le raccourci existe déjà sur le bureau, et s'il existe, supprimez-le
+desktop = Path(os.path.join(os.path.expanduser('~'), 'Desktop'))
+shortcut_path = desktop / 'Gestion_Budget.lnk'
 
-# Copiez l'exécutable sur le bureau de l'utilisateur
-output_folder = os.path.join(download_path, 'dist')
-executable_path = os.path.join(output_folder, 'main.exe')
-desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+if os.path.exists(shortcut_path):
+    os.remove(shortcut_path)
 
-shutil.copy2(executable_path, desktop_path)
+# Créez un raccourci sur le bureau de l'utilisateur
+target_path = os.path.join(install_path, 'main.exe')
+
+shell = win32com.client.Dispatch("WScript.Shell")
+shortcut = shell.CreateShortCut(str(shortcut_path))
+shortcut.TargetPath = target_path
+shortcut.WorkingDirectory = install_path
+shortcut.IconLocation = target_path
+shortcut.save()
 
 print("Installation terminée et l'exécutable est sur le bureau !")
 
-# Ajoutez cette ligne pour maintenir la console ouverte
+# Maintenez la console ouverte jusqu'à ce que l'utilisateur appuie sur Entrée
 input("Appuyez sur Entrée pour quitter...")
-
